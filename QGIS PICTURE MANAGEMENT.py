c01bb46ad12b56d9171d59c7ba7b89c8cd441d79
billymosis <57342180+billymosis@users.noreply.github.com>
2019-11-04 15:53:23 +0700

    Update QGIS PICTURE MANAGEMENT.py

--- QGIS PICTURE MANAGEMENT.py
+++ QGIS PICTURE MANAGEMENT.py
@@ -1,6 +1,11 @@
+import os
+import shutil
+from qgis.utils import iface
+
+layer = iface.activeLayer()
 selection = layer.selectedFeatures()
 print('Jumlat File Ter-select = '+ str(len(selection)))
-dest = 'D:/HAHAHAHA/'
+dest = 'D:/A/BTL-4/'
 for feature in selection:
     os.makedirs(os.path.dirname(dest), exist_ok=True)
     shutil.copy(feature[0],dest)
