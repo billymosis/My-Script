a96fac85b1a26b0948f2f4b07b0389d1c0e78d7f
Billy Mosis Priambodo <57342180+billymosis@users.noreply.github.com>
2020-09-07 01:56:36 +0700

    Add Folder Variable

--- QGIS PICTURE MANAGEMENT.py
+++ QGIS PICTURE MANAGEMENT.py
@@ -5,7 +5,8 @@ from qgis.utils import iface
 layer = iface.activeLayer()
 selection = layer.selectedFeatures()
 print('Jumlat File Ter-select = '+ str(len(selection)))
-dest = 'D:/A/BTL-4/'
+folder = 'Ruas 42'
+dest = f'D:/mrican/Inventori/Sentul/{folder}/'
 for feature in selection:
     os.makedirs(os.path.dirname(dest), exist_ok=True)
     shutil.copy(feature[0],dest)
